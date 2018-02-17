94fe0d9d50febba7cd2c1bf9faa202f5a5fbb253
Kenichi Saito <hajime@studiohff.net>
2018-02-17 18:12:30 +0900

    OkCancelDialogを追加

---
+++ src/component/okCancelDialog.vue
@@ -0,0 +1,32 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <div class="modal-header">
+                        <slot name="header">default header</slot>
+                    </div>
+
+                    <div class="modal-body">
+                        <slot name="body">default body</slot>
+                    </div>
+
+                    <div class="modal-footer">
+                        <slot name="footer">default footer</slot>
+                        <button class="modal-default-button" v-on:click="$emit('ok')">OK</button>
+                        <button class="modal-default-button" v-on:click="$emit('cancel')">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component
+    export default class OkCancelDialog extends Vue {
+    }
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
 import Vue from "vue";
+
+import OkCancelDialog from "./component/okCancelDialog.vue";
 import OkDialog from "./component/okDialog.vue";
 
 const app = new Vue({
     el: "#app",
     data: {
-        showModal: false
+        showModal: false,
+        showOkCancel: false,
+        showOkCancelClosedBy: "",
     },
     components: {
         "ok-dialog": OkDialog,
+        "ok-cancel-dialog": OkCancelDialog,
+    },
+    methods: {
+        onOKCancelDialogOpen() {
+            this.showOkCancel = true;
+            this.showOkCancelClosedBy = "";
+        },
+        onOkCancelDialogClosedByOk() {
+            this.showOkCancel = false;
+            this.showOkCancelClosedBy = "OKが押されました";
+        },
+        onOkCancelDialogClosedByCancel() {
+            this.showOkCancel = false;
+            this.showOkCancelClosedBy = "Cancelが押されました";
+        },
     },
 });
